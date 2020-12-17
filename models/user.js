da5b62ef296b0755a2451743b529cd2a474583b7
Mitch <mitch.mathias96@gmail.com>
2020-12-17 10:27:16 -0500

    hbs

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 const bcrypt = require("bcryptjs");
 const { INTEGER } = require("sequelize");
 // Creating our User model
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   const User = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
     email: {
@@ -22,9 +22,10 @@ module.exports = function(sequelize, DataTypes) {
     clearance: {
       type: INTEGER,
       allowNull: false
-  });
+    }
+  })
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
